9fc03b9b7d542366c189ca114c1224ed649fa6c6
p-zielinski <82354717+p-zielinski@users.noreply.github.com>
2023-08-29 16:00:20 +0200

    done..

--- packages/sdk/src/types/Exports.ts
+++ packages/sdk/src/types/Exports.ts
@@ -65,8 +65,10 @@ export interface ExportsCreateResponseVoucher {
 	parameters: {
 		fields?: ExportVoucherFields[]
 		filters?: {
-			junction?: string
-		} & Record<string, any>
+			junction?: Junction
+			conditions?: Record<ExportVoucherFields, Record<FiltersCondition, any>>
+			order?: ExportVoucherOrder
+		}
 	}
 }
 
@@ -75,8 +77,10 @@ export interface ExportsCreateResponseRedemption {
 	parameters: {
 		fields?: ExportRedemptionFields[]
 		filters?: {
-			junction?: string
-		} & Record<string, any>
+			junction?: Junction
+			conditions?: Record<ExportRedemptionFields, Record<FiltersCondition, any>>
+			order?: ExportRedemptionOrder
+		}
 	}
 }
 
@@ -85,8 +89,10 @@ export interface ExportsCreateResponseCustomer {
 	parameters: {
 		fields?: ExportCustomerFields[]
 		filters?: {
-			junction?: string
-		} & Record<string, any>
+			junction?: Junction
+			conditions?: Record<ExportCustomerFields, Record<FiltersCondition, any>>
+			order?: ExportCustomerOrder
+		}
 	}
 }
 
@@ -95,8 +101,10 @@ export interface ExportsCreateResponsePublication {
 	parameters: {
 		fields?: ExportPublicationFields[]
 		filters?: {
-			junction?: string
-		} & Record<string, any>
+			junction?: Junction
+			conditions?: Record<ExportPublicationFields, Record<FiltersCondition, any>>
+			order?: ExportPublicationOrder
+		}
 	}
 }
 
@@ -105,8 +113,10 @@ export interface ExportsCreateResponseOrder {
 	parameters: {
 		fields?: ExportOrderFields[]
 		filters?: {
-			junction?: string
-		} & Record<string, any>
+			junction?: Junction
+			conditions?: Record<ExportOrderFields, Record<FiltersCondition, any>>
+			order?: ExportOrderOrder
+		}
 	}
 }
 
@@ -115,8 +125,10 @@ export interface ExportsCreateResponsePointsExpiration {
 	parameters: {
 		fields?: ExportPointsExpirationFields[]
 		filters?: {
-			junction?: string
-		} & Record<string, any>
+			junction?: Junction
+			conditions?: Record<ExportPointsExpirationFields, Record<FiltersCondition, any>>
+			order?: ExportPointsExpirationOrder
+		}
 	}
 }
 
@@ -125,8 +137,10 @@ export interface ExportsCreateVoucherTransactionsExpiration {
 	parameters: {
 		fields?: ExportVoucherTransactionsFields[]
 		filters?: {
-			junction?: string
-		} & Record<string, any>
+			junction?: Junction
+			conditions?: Record<ExportVoucherTransactionsFields, Record<FiltersCondition, any>>
+			order?: ExportVoucherTransactionsOrder
+		}
 	}
 }
 
@@ -144,6 +158,35 @@ export interface ListExports {
 	exports: ExportResourceResponse[]
 	total: number
 }
+
+export type Junction = 'and' | 'AND' | 'or' | 'OR'
+
+export type FiltersCondition =
+	| '$in'
+	| '$not_in'
+	| '$is'
+	| '$is_days_ago'
+	| '$is_days_in_future'
+	| '$is_not'
+	| '$has_value'
+	| '$is_unknown'
+	| '$contains'
+	| '$not_contain'
+	| '$starts_with'
+	| '$ends_with'
+	| '$more_than'
+	| '$less_than'
+	| '$more_than_ago'
+	| '$less_than_ago'
+	| '$more_than_future'
+	| '$less_than_future'
+	| '$more_than_equal'
+	| '$less_than_equal'
+	| '$after'
+	| '$before'
+	| '$count'
+	| '$count_less'
+	| '$count_more'
 
 export type ExportCustomerFields =
 	| 'name'
@@ -177,6 +220,39 @@ export type ExportCustomerFields =
 	| 'metadata'
 	| 'birthdate'
 
+export type ExportCustomerOrder =
+	| ExportCustomerFields
+	| '-name'
+	| '-id'
+	| '-description'
+	| '-email'
+	| '-source_id'
+	| '-created_at'
+	| '-address_city'
+	| '-address_state'
+	| '-address_line_1'
+	| '-address_line_2'
+	| '-address_country'
+	| '-address_postal_code'
+	| '-redemptions_total_redeemed'
+	| '-redemptions_total_failed'
+	| '-redemptions_total_succeeded'
+	| '-redemptions_total_rolled_back'
+	| '-redemptions_total_rollback_failed'
+	| '-redemptions_total_rollback_succeeded'
+	| '-orders_total_amount'
+	| '-orders_total_count'
+	| '-orders_average_amount'
+	| '-orders_last_order_amount'
+	| '-orders_last_order_date'
+	| '-loyalty_points'
+	| '-loyalty_referred_customers'
+	| '-updated_at'
+	| '-phone'
+	| '-birthday'
+	| '-metadata'
+	| '-birthdate'
+
 export type ExportPublicationFields =
 	| 'voucher_code'
 	| 'customer_id'
@@ -186,6 +262,17 @@ export type ExportPublicationFields =
 	| 'campaign'
 	| 'is_winner'
 	| 'metadata'
+
+export type ExportPublicationOrder =
+	| ExportPublicationFields
+	| '-voucher_code'
+	| '-customer_id'
+	| '-customer_source_id'
+	| '-date'
+	| '-channel'
+	| '-campaign'
+	| '-is_winner'
+	| '-metadata'
 
 export type ExportRedemptionFields =
 	| 'id'
@@ -205,6 +292,26 @@ export type ExportRedemptionFields =
 	| 'failure_code'
 	| 'failure_message'
 	| 'metadata'
+
+export type ExportRedemptionOrder =
+	| ExportRedemptionFields
+	| '-id'
+	| '-object'
+	| '-date'
+	| '-voucher_code'
+	| '-campaign'
+	| '-promotion_tier_id'
+	| '-customer_id'
+	| '-customer_source_id'
+	| '-customer_name'
+	| '-tracking_id'
+	| '-order_amount'
+	| '-gift_amount'
+	| '-loyalty_points'
+	| '-result'
+	| '-failure_code'
+	| '-failure_message'
+	| '-metadata'
 
 export type ExportVoucherFields =
 	| 'code'
@@ -238,6 +345,39 @@ export type ExportVoucherFields =
 	| 'discount_unit_effect'
 	| 'customer_source_id'
 
+export type ExportVoucherOrder =
+	| ExportVoucherFields
+	| '-code'
+	| '-voucher_type'
+	| '-value'
+	| '-discount_type'
+	| '-campaign'
+	| '-category'
+	| '-start_date'
+	| '-expiration_date'
+	| '-gift_balance'
+	| '-loyalty_balance'
+	| '-redemption_quantity'
+	| '-redemption_count'
+	| '-active'
+	| '-qr_code'
+	| '-bar_code'
+	| '-metadata'
+	| '-id'
+	| '-is_referral_code'
+	| '-created_at'
+	| '-updated_at'
+	| '-validity_timeframe_interval'
+	| '-validity_timeframe_duration'
+	| '-validity_day_of_week'
+	| '-discount_amount_limit'
+	| '-campaign_id'
+	| '-additional_info'
+	| '-customer_id'
+	| '-discount_unit_type'
+	| '-discount_unit_effect'
+	| '-customer_source_id'
+
 export type ExportOrderFields =
 	| 'id'
 	| 'source_id'
@@ -253,7 +393,32 @@ export type ExportOrderFields =
 	| 'referrer_id'
 	| 'metadata'
 
+export type ExportOrderOrder =
+	| ExportOrderFields
+	| '-id'
+	| '-source_id'
+	| '-created_at'
+	| '-updated_at'
+	| '-status'
+	| '-amount'
+	| '-discount_amount'
+	| '-items_discount_amount'
+	| '-total_discount_amount'
+	| '-total_amount'
+	| '-customer_id'
+	| '-referrer_id'
+	| '-metadata'
+
 export type ExportPointsExpirationFields = 'id' | 'campaign_id' | 'voucher_id' | 'points' | 'status' | 'expires_at'
+
+export type ExportPointsExpirationOrder =
+	| ExportPointsExpirationFields
+	| '-id'
+	| '-campaign_id'
+	| '-voucher_id'
+	| '-points'
+	| '-status'
+	| '-expires_at'
 
 export type ExportVoucherTransactionsFields =
 	| 'id'
@@ -268,3 +433,18 @@ export type ExportVoucherTransactionsFields =
 	| 'related_transaction_id'
 	| 'created_at'
 	| 'details'
+
+export type ExportVoucherTransactionsOrder =
+	| ExportVoucherTransactionsFields
+	| '-id'
+	| '-campaign_id'
+	| '-voucher_id'
+	| '-type'
+	| '-source_id'
+	| '-reason'
+	| '-source'
+	| '-balance'
+	| '-amount'
+	| '-related_transaction_id'
+	| '-created_at'
+	| '-details'
