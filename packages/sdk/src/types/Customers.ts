d798ce732682ff36fd23e560dfacda9111bb92fe
p-zielinski <82354717+p-zielinski@users.noreply.github.com>
2023-08-29 10:59:09 +0200

    done

--- packages/sdk/src/types/Customers.ts
+++ packages/sdk/src/types/Customers.ts
@@ -1,3 +1,21 @@
+export interface CustomerPermanentDeletion {
+	id: string
+	created_at: string
+	related_object_id: string
+	related_object: 'customer'
+	status: string
+	data_json: {
+		events: number
+		customer_events: number
+		daily_event?: number
+		segments: number
+		orders: number
+		order_events: number
+		customer: 1
+	}
+	object: 'pernament_deletion'
+}
+
 export interface SimpleCustomer {
 	id: string
 	name?: string
@@ -84,6 +102,10 @@ export interface CustomerRequest {
 	phone?: string
 }
 
+export type CustomerBasicData = Omit<CustomerRequest, 'id' | 'source_id'>
+
+export type UpdateCustomersInBulk = (CustomerBasicData & { source_id: string })[]
+
 export interface CustomersCommonListRequest {
 	limit?: number
 	page?: number
@@ -136,3 +158,8 @@ export type CustomersUpdateParams = CustomerRequest & IdOrSourceId
 export type CustomersUpdateResponse = CustomerObject | CustomerUnconfirmed
 
 export type CustomersUpdateConsentsBody = Record<string, boolean>
+
+export interface UpdateCustomersMetadataInBulk {
+	source_ids: string[]
+	metadata: Record<string, any>
+}
